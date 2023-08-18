bd8fb1c551d331bdf7ede023a9b3553551fd3075
SaucyBoi21 <72944967+SaucyBoi21@users.noreply.github.com>
2023-08-18 10:23:13 -0400

    added endpoints

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -1,22 +1,25 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL;
+const express = require('express')
+const mongoose = require('mongoose')
+const mongoString = process.env.DATABASE_URL
+const routes = require('./routes/routes.js')
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
+mongoose.connect(mongoString)
+const database = mongoose.connection
 
 database.on('error', (error) => {
     console.log(error)
 })
 
 database.once('connected', () => {
-    console.log('Database Connected');
+    console.log('Database Connected')
 })
-const app = express();
 
-app.use(express.json());
+const app = express()
+
+app.use(express.json())
+app.use('/api', routes)
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
